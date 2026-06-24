f40b9bbec648e00d519e32f5a6a4b353c72fb44f
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2026-06-24 20:41:34 +0630

    Add token rotation for refresh tokens

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
+require('dotenv').config();
 const express = require('express');
-
+const jwt = require('jsonwebtoken');
 const app = express();
-const PORT = 3000;
-app.use(express.json());
+const cookieParser = require('cookie-parser');
 
+app.use(cookieParser());
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+const JWT_SECRET = process.env.JWT_SECRET;
+const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+console.log()
 const users = [
     {
         id: 1,
         name: "Tun Tun",
         email: "tuntun@example.com",
         phoneNumber: "09123456789",
-        password: "password123"
+        password: "password123",
+        refreshToken: "",
     },
     {
         id: 2,
         name: "Su Su",
         email: "susu@example.com",
         phoneNumber: "09987654321",
-        password: "password456"
+        password: "password456",
+        refreshToken: ""
     },
     {
         id: 3,
         name: "Aung Aung",
         email: "aung@example.com",
         phoneNumber: "09456789123",
-        password: "password789"
+        password: "password789",
+        refreshToken: ""
     }
 ];
 
@@ -47,7 +56,8 @@ app.post('/auth/register', (req, res) => {
         name,
         email,
         phoneNumber,
-        password
+        password,
+        refreshToken: ""
     }
     users.push(newUser);
     console.log(users);
@@ -66,9 +76,81 @@ app.post('/auth/login', (req, res) => {
     if (!foundUser) return res.status(404).json({ message: "You have to register first" });
     const isMatch = foundUser.password === password;
     if (!isMatch) return res.status(401).json({ message: "Password don't match" });
+    const { refreshToken: hidden, password: _, ...rest } = foundUser;
+
+    const accesstoken = jwt.sign({
+        id: foundUser.id,
+        email: foundUser.email
+    }, JWT_SECRET, {
+        expiresIn: '30s'
+    });
+
+    const refreshToken = jwt.sign({
+        id: foundUser.id,
+        email: foundUser.email
+    },
+        REFRESH_SECRET,
+        {
+            expiresIn: '7d'
+        })
+
+    foundUser.refreshToken = refreshToken;
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    })
+
     return res.status(200).json({
         message: "Login Sucessful",
-        user: foundUser,
+        user: rest,
+        accesstoken
+    })
+})
+
+app.get('/auth/logout', (req, res) => {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) return res.sendStatus(204);
+    const foundUser = users.find(user => user.refreshToken === refreshToken);
+    if (foundUser) {
+        foundUser.refreshToken = ""
+    }
+    res.clearCookie('refreshToken');
+
+    return res.status(200).json({
+        message: "Logout successfull"
+    })
+})
+
+app.get('/get/users', (req, res) => {
+    console.log(users)
+    res.status(200).json({
+        message: 'This is all users',
+        users
+    })
+})
+app.post('/auth/refresh', (req, res) => {
+    const refreshToken = req.cookies.refreshToken;
+    console.log(refreshToken);
+    if (!refreshToken) return res.status(401).json({ message: "No refresh token" });
+    const foundUser = users.find(user => user.refreshToken === refreshToken);
+    if (!foundUser) return res.status(401).json({ message: "Refresh token not recognized" });
+
+    jwt.verify(refreshToken, REFRESH_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({
+                message: 'Invalid refresh token'
+            });
+        }
+        const accesstoken = jwt.sign({
+            id: decoded.id,
+            email: decoded.email
+        }, JWT_SECRET, {
+            expiresIn: '30s',
+        })
+
+        return res.status(200).json({ accesstoken });
     })
 })
 
