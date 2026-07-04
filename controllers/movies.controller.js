db84187f3742ab4bcd0d5be29b81a17a79c0a5f4
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2026-07-04 12:04:27 +0630

    refactor: move movie route logic into controllers

---
+++ controllers/movies.controller.js
@@ -0,0 +1,288 @@
+import prisma from "../lib/prisma.js";
+import redis from "../lib/redis.js"
+import { getMovieRedisKey } from "../lib/ulti.js";
+
+export const getAllMovies = async (req, res) => {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const movieKey = getMovieRedisKey(page, limit);
+
+    try {
+        const cacheMovies = await redis.get(movieKey);
+
+        if (cacheMovies) {
+            console.log("cache hit");
+            return res.status(200).json(JSON.parse(cacheMovies));
+        }
+
+        const totalMovies = await prisma.movie.count();
+
+        const movies = await prisma.movie.findMany({
+            skip: (page - 1) * limit,
+            take: limit,
+            orderBy: {
+                releaseDate: "desc",
+            },
+        });
+
+        const totalPages = Math.ceil(totalMovies / limit);
+
+        const data = {
+            page,
+            limit,
+            movies,
+            totalPages,
+            totalMovies,
+        };
+
+        console.log("cache miss");
+
+        await redis.set(movieKey, JSON.stringify(data), {
+            EX: 60,
+        });
+
+        return res.status(200).json(data);
+
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Something went wrong",
+        });
+    }
+};
+
+export const createMovie = async (req, res) => {
+    const {
+        title,
+        description,
+        releaseDate,
+        duration,
+        posterUrl,
+        backdropUrl,
+        language,
+        genreIds
+    } = req.body;
+
+    if (!title || !description) {
+        return res.status(400).json({
+            message: "Title and description are required"
+        });
+    }
+
+    try {
+        const movie = await prisma.movie.create({
+            data: {
+                title,
+                description,
+                releaseDate: releaseDate ? new Date(releaseDate) : null,
+                duration,
+                posterUrl,
+                backdropUrl,
+                language,
+            },
+        });
+
+        if (genreIds?.length) {
+            await prisma.movieGenre.createMany({
+                data: genreIds.map(id => ({
+                    movieId: movie.id,
+                    genreId: id,
+                }))
+            });
+        }
+
+        return res.status(201).json({
+            message: "Movie created successfully",
+            movie,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Failed to create movie",
+        });
+    }
+};
+
+export const getMovie = async (req, res) => {
+    const movieId = Number(req.params.id);
+
+    if (isNaN(movieId)) {
+        return res.status(400).json({
+            message: "Invalid movie id"
+        });
+    }
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id: movieId
+            },
+            include: {
+                movieGenres: {
+                    include: {
+                        genre: true
+                    }
+                }
+            }
+        });
+
+        if (!movie) {
+            return res.status(404).json({
+                message: "Movie not found"
+            });
+        }
+
+        return res.status(200).json({
+            ...movie, movieGenres: movie.movieGenres.map(mg => {
+                return mg.genre.name
+            })
+        });
+    } catch (err) {
+        console.error(err);
+
+        return res.status(500).json({
+            message: "Something went wrong"
+        });
+    }
+};
+
+export const updateMovie = async (req, res) => {
+    try {
+        const movieId = Number(req.params.id);
+        if (isNaN(movieId)) {
+            return res.status(400).json({
+                message: "Invalid movie id",
+            });
+        }
+
+        const existingMovie = await prisma.movie.findUnique({
+            where: { id: movieId },
+        });
+
+        if (!existingMovie) {
+            return res.status(404).json({
+                message: "Movie not found",
+            });
+        }
+
+        const {
+            title,
+            description,
+            releaseDate,
+            duration,
+            posterUrl,
+            backdropUrl,
+            language,
+        } = req.body;
+
+        const updatedMovie = await prisma.movie.update({
+            where: { id: movieId },
+            data: {
+                ...(title !== undefined && { title }),
+                ...(description !== undefined && { description }),
+                ...(releaseDate !== undefined && {
+                    releaseDate: releaseDate ? new Date(releaseDate) : null,
+                }),
+                ...(duration !== undefined && { duration }),
+                ...(posterUrl !== undefined && { posterUrl }),
+                ...(backdropUrl !== undefined && { backdropUrl }),
+                ...(language !== undefined && { language }),
+            },
+        });
+
+        return res.status(200).json({
+            message: "Movie updated successfully",
+            movie: updatedMovie,
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal server error",
+        });
+    }
+};
+
+export const deleteMovie = async (req, res) => {
+    const movieId = Number(req.params.id);
+
+    if (isNaN(movieId)) {
+        return res.status(400).json({
+            message: "Invalid movie id",
+        });
+    }
+
+    try {
+        await prisma.movie.delete({
+            where: { id: movieId },
+        });
+
+        return res.status(200).json({
+            message: "Movie deleted successfully",
+        });
+
+    } catch (err) {
+
+        if (err?.code === "P2025") {
+            return res.status(404).json({
+                message: "Movie not found",
+            });
+        }
+
+        return res.status(500).json({
+            message: "Something went wrong",
+        });
+    }
+};
+
+export const reviewMovie = async (req, res) => {
+    const movieId = Number(req.params.movieId);
+    const userId = Number(req.body.userId);
+    const { rating, comment } = req.body;
+
+    if (isNaN(movieId)) {
+        return res.status(400).json({
+            message: "Invalid movie id"
+        });
+    }
+
+    if (isNaN(userId)) {
+        return res.status(400).json({
+            message: "Invalid user id"
+        });
+    }
+
+    if (rating === undefined || !comment) {
+        return res.status(400).json({
+            message: "Rating and comment are required"
+        });
+    }
+
+    try {
+        await prisma.review.create({
+            data: {
+                rating,
+                comment,
+                movieId,
+                userId,
+            }
+        });
+
+        return res.status(201).json({
+            message: "Review added successfully"
+        });
+
+    } catch (error) {
+        if (error.code === 'P2002') {
+            return res.status(400).json({
+                message: "You already reviewed this movie"
+            });
+        }
+
+        return res.status(500).json({
+            message: "Something went wrong"
+        });
+    }
+};
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
 import movieRoutes from './routes/movie.route.js'
-import { verifyToken } from "./controllers/middlewares/auth.middleware.js";
+import { verifyToken } from "./middlewares/auth.middleware.js";
 
 const app = express();
 
@@ -26,4 +26,3 @@ app.use('/movies', movieRoutes)
 app.listen(PORT, () => {
     console.log("Server is running at", PORT);
 })
-console.log("hi")
