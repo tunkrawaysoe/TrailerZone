4cb8356eaa76b6f75c8896729f3625455d88a5f2
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2026-07-13 12:45:44 +0630

    feat: add dynamic pagination and refetch movies on page change

--- frontend/src/components/PaginationButton.jsx
+++ frontend/src/components/PaginationButton.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 const PaginationButton = ({ totalPagesArray, page, setPage }) => {
+  const visiblePages = 7;
+  const totalPages = totalPagesArray.length;
+  let start = Math.max(1, page - Math.floor(visiblePages / 2));
+  let end = start + visiblePages - 1;
+  
+  if (end > totalPages) {
+    end = totalPages;
+    start = Math.max(1, end - visiblePages + 1);
+  }
+  const pages = totalPagesArray.slice(start - 1, end);
+
   return (
     <div className="pagination">
       <button
@@ -11,10 +22,12 @@ const PaginationButton = ({ totalPagesArray, page, setPage }) => {
         Previous
       </button>
 
-      {totalPagesArray.map((pageNumber) => (
+      {pages.map((pageNumber) => (
         <button
-          className={`page-number ${page === pageNumber ? "active" : ""}`}
           key={pageNumber}
+          className={`page-number ${
+            page === pageNumber ? "active" : ""
+          }`}
           onClick={() => setPage(pageNumber)}
         >
           {pageNumber}
@@ -23,6 +36,7 @@ const PaginationButton = ({ totalPagesArray, page, setPage }) => {
 
       <button
         className="page-btn"
+        disabled={page === totalPages}
         onClick={() => setPage(page + 1)}
       >
         Next
--- frontend/src/pages/Movies/Movies.jsx
+++ frontend/src/pages/Movies/Movies.jsx
@@ -1,32 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { MainCard } from "../../components/MainCard";
-import { useState } from "react";
-import "./Movies.css";
 import PaginationButton from "../../components/PaginationButton";
+import "./Movies.css";
 
 const Movies = () => {
   const [allMovies, setAllMovies] = useState([]);
   const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
+  const [totalPages, setTotalPages] = useState(30);
   const totalPagesArray = [];
-  console.log(totalPagesArray);
+
   for (let i = 1; i <= totalPages; i++) {
     totalPagesArray.push(i);
   }
 
   async function fetchAllData() {
     const response = await fetch(`http://localhost:3000/movies?page=${page}`);
-    const movies = await response.json();
-    setTotalPages(movies.totalPages);
-    setAllMovies(movies.movies);
+    const data = await response.json();
+
+    setAllMovies(data.movies);
+    // setTotalPages(data.totalPages);
   }
 
   useEffect(() => {
     fetchAllData();
-  }, []);
+  }, [page]); 
+
   return (
     <>
       <MainCard movies={allMovies} />
+
       <PaginationButton
         totalPagesArray={totalPagesArray}
         page={page}
