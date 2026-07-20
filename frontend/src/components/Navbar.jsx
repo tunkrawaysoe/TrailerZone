b01ed258c5155dd8016aaa242a1e80a7ab961a14
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2026-07-20 22:01:22 +0630

    feat: add logout button and clear authentication state

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { logOut } from "../redux/authSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Navbar() {
+  const accessToken = useSelector((state) => state.auth.accessToken);
+  const dispatch = useDispatch();
+  async function handleLogout() {
+    try {
+      await fetch("http://localhost:3000/auth/logout", {
+        credentials: "include",
+      });
+      dispatch(logOut());
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  }
+
   return (
     <header>
       <nav className="navbar">
@@ -12,6 +27,13 @@ export default function Navbar() {
           <li>
             <Link to={"/watchLists"}>WatchList</Link>
           </li>
+          {accessToken && (
+            <li>
+              <button className="logout-btn" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
--- frontend/src/pages/WatchLists/WatchListPage.jsx
+++ frontend/src/pages/WatchLists/WatchListPage.jsx
@@ -1,11 +1,23 @@
 import { useSelector } from "react-redux";
 import { MainCard } from "../../components/MainCard";
-
+import "./WatchListPage.css";
 const WatchListPage = () => {
   const movies = useSelector((state) => state.watchList.movies);
+  const { accessToken, loading } = useSelector((state) => state.auth);
+  if (loading) {
+    return <div className="section">Loading...</div>;
+  }
+  if (!accessToken) {
+    return (
+      <div className="login-text">
+        <h2>You have to register or log in first.</h2>
+      </div>
+    );
+  }
+
   return (
     <div className="section">
-      <MainCard movies={movies} title={"Watch Lists"} />
+      <MainCard movies={movies} title="Watch Lists" />
     </div>
   );
 };
